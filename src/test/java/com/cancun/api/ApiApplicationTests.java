59cf12e2bb29a07e136b3c45b0e6cee254aab223
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-06-25 23:55:16 +0200

    update post test

--- src/test/java/com/cancun/api/ApiApplicationTests.java
+++ src/test/java/com/cancun/api/ApiApplicationTests.java
@@ -1,9 +1,18 @@
 package com.cancun.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.cancun.api.model.Reservation;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -44,17 +53,34 @@ class ApiApplicationTests {
 	}
 	
 	@Test
-	void testUserCanPlaceReservation() {
-		//
+	void testUserCanPlaceReservation() throws Exception {
+		Reservation reservationToPost = new Reservation();
+		Reservation reservationToReturn = new Reservation();
+        
+		mockMvc.perform(
+				post("/api/reservations")
+				.contentType(MediaType.APPLICATION_JSON)
+		        .content(asJsonString(reservationToPost)))
+		.andExpect(status().isOk());
 	}
 
 	@Test
-	void testUserCanCancelReservation() {
-		//
+	void testUserCanCancelReservation() throws Exception {
+		mockMvc.perform(delete("/api/reservations/1"))
+		.andExpect(status().isOk());
 	}
 	
 	@Test
-	void testUserCanUpdateReservation() {
-		//
+	void testUserCanUpdateReservation() throws Exception {
+		mockMvc.perform(put("/api/reservations/1"))
+		.andExpect(status().isOk());
 	}
+	
+	static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
